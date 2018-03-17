6f2923a91374c8cfea6a05dc9e17f3a0b51a93a1
Olu Meduoye <olu.meduoye@tvplayer.com>
2018-03-17 12:12:22 +0000

    initial build for backend architecture

---
+++ backend/server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dbConfig from './config/db';
+
+const app = express();
+
+// Database
+
+dbConfig();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`App is listening on port: ${PORT}`);
+  }
+});
