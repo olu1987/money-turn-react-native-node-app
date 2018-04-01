89e78c497f2842fdaa366134a57a35ce43a79e73
Olu Meduoye <olu.meduoye@tvplayer.com>
2018-04-01 20:34:27 +0100

    Adding bootstrap 4 and  rendering 1st group

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
-        "linebreak-style": 0
+        "linebreak-style": 0,
+        "class-methods-use-this":0,
+        "react/jsx-filename-extension":0
     }
 };
--- frontend/src/containers/saving_groups.js
+++ frontend/src/containers/saving_groups.js
@@ -4,44 +4,47 @@ import { bindActionCreators } from 'redux';
 import actions from '../actions';
 
 class SavingGroups extends Component {
-    constructor(props){
-        super(props);
-        this.props.fetchAllSavingGroups();
-    }
-
-    componentDidMount() {
-
-    }
-
-    render() {
-        console.log(this.props);
-        return (
-          <div>Hello Money Turns</div>
-        )
-    }
-
-    renderGroups(group){
-        const name = cityData.city.name;
-
-        return(
-            <tr key={name}>
-                <td>{name}</td>
-                <td>
-                </td>
-            </tr>
-        )
-    }
-}
-
-const mapStateToProps = state => {
-    return {
-        savingGroups: state.savingGroups
-    }
+  constructor(props) {
+    super(props);
+    this.props.fetchAllSavingGroups();
   }
 
+  componentDidMount() {
+
+  }
+
+  renderGroups(group) {
+    const { title, description } = group;
+
+    return (
+      <div>
+        <h1>{ title }</h1>
+        <p>{ description }</p>
+        <div>
+          <a href="#" className="btn btn-lg btn-primary text-uppercase">Join group</a>
+          <a href="#" className="btn btn-lg btn-danger text-uppercase ml-1">Leave group</a>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    console.log();
+    return (
+      <div>{this.props.savingGroups.map(this.renderGroups)}</div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    savingGroups: state.savingGroups,
+  };
+};
+
 function mapDispatchToProps(dispatch) {
-    const fetchAllSavingGroups = actions.fetchAllSavingGroups;
-    return bindActionCreators( { fetchAllSavingGroups } , dispatch)
+  const { fetchAllSavingGroups } = actions;
+  return bindActionCreators({ fetchAllSavingGroups }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SavingGroups);
